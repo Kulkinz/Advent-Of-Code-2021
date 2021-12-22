e6100449de3a98ad89df77e86b49b5953fccdc3f
Kulkinz <cool@coolspages.com>
2021-12-21 23:09:19 -0800

    Completed Day 22 - 00:12:49 - 01:26:16 - Started late, times adjusted to account for late start

--- Day22/src/App.java
+++ Day22/src/App.java
@@ -1,7 +1,6 @@
 import java.io.FileNotFoundException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class App {
 
@@ -19,7 +18,7 @@ public class App {
 
         List<String> data = parser.parseDataStr();
 
-        Map<Coordinate, Boolean> reactor = new HashMap<>();
+        List<Cube> cubes = new ArrayList<>();
 
         for (String string : data) {
             String[] split = string.split(" ");
@@ -31,24 +30,177 @@ public class App {
             String[] splitY = coordSplit[1].split("\\.\\.");
             String[] splitZ = coordSplit[2].split("\\.\\.");
 
+            Coordinate lesserPoint = new Coordinate(Integer.parseInt(splitX[0]), Integer.parseInt(splitY[0]), Integer.parseInt(splitZ[0]));
+            Coordinate greaterPoint = new Coordinate(Integer.parseInt(splitX[1]), Integer.parseInt(splitY[1]), Integer.parseInt(splitZ[1]));
+
+            Cube cube = new Cube(lesserPoint, greaterPoint);
             boolean state = split[0].equals("on");
-            for (int x = (Math.max(Integer.parseInt(splitX[0]), -50)); x <= (Math.min(Integer.parseInt(splitX[1]), 50)); x++) {
-                for (int y = (Math.max(Integer.parseInt(splitY[0]), -50)); y <= (Math.min(Integer.parseInt(splitY[1]), 50)); y++) {
-                    for (int z = (Math.max(Integer.parseInt(splitZ[0]), -50)); z <= (Math.min(Integer.parseInt(splitZ[1]), 50)); z++) {
-                        Coordinate coordinate = new Coordinate(x, y, z);
-                        reactor.put(coordinate, state);
+
+            // add
+            if (state) {
+                ArrayList<Cube> queue = new ArrayList<>();
+                queue.add(cube);
+                for (Cube listCube : cubes) {
+                    ArrayList<Cube> result = new ArrayList<>();
+                    for (Cube piecesOfCube : queue) {
+                        result.addAll(piecesOfCube.subtract(listCube));
                     }
+                    queue = result;
                 }
+                cubes.addAll(queue);
+            } else { // subtract
+
+                ArrayList<Cube> resultingCubes = new ArrayList<>();
+                for (Cube listCube : cubes) {
+                    resultingCubes.addAll(listCube.subtract(cube));
+                }
+                cubes = resultingCubes;
             }
+
         }
 
-        long acc = 0;
-        for (boolean state : reactor.values()) {
-            if (state) {
-                acc++;
-            }
+        long acc = 0L;
+        for (Cube cube : cubes) {
+            acc += cube.area();
         }
         System.out.println(acc);
+    }
+
+    private class Cube {
+        Coordinate lesserPoint;
+        Coordinate greaterPoint;
+
+        public Cube(Coordinate lesserPoint, Coordinate greaterPoint) {
+            this.lesserPoint = new Coordinate(lesserPoint);
+            this.greaterPoint = new Coordinate(greaterPoint);
+        }
+
+        public long area() {
+            return ((long) (greaterPoint.x + 1 - lesserPoint.x)) * ((long) (greaterPoint.y + 1 - lesserPoint.y)) * ((long) (greaterPoint.z + 1 - lesserPoint.z));
+        }
+
+        public List<Cube> subtract(Cube opposing) {
+            List<Cube> newCubes = new ArrayList<>();
+            Cube cubeHelper;
+            Coordinate coordinateHelper;
+
+            if ((lesserPoint.x < opposing.lesserPoint.x && greaterPoint.x < opposing.lesserPoint.x) ||
+                    (lesserPoint.x > opposing.greaterPoint.x && greaterPoint.x > opposing.greaterPoint.x) ||
+                    (lesserPoint.y < opposing.lesserPoint.y && greaterPoint.y < opposing.lesserPoint.y) ||
+                    (lesserPoint.y > opposing.greaterPoint.y && greaterPoint.y > opposing.greaterPoint.y) ||
+                    (lesserPoint.z < opposing.lesserPoint.z && greaterPoint.z < opposing.lesserPoint.z) ||
+                    (lesserPoint.z > opposing.greaterPoint.z && greaterPoint.z > opposing.greaterPoint.z)) {
+                newCubes.add(this);
+                return newCubes; // they do not intersect, no subtraction needed
+            }
+
+            if (lesserPoint.x < opposing.lesserPoint.x) {
+                    // |x-----|o--x-----o
+                    coordinateHelper = new Coordinate(greaterPoint);
+                    coordinateHelper.x = opposing.lesserPoint.x - 1;
+                    cubeHelper = new Cube(lesserPoint, coordinateHelper);
+                    newCubes.add(cubeHelper);
+                    coordinateHelper.x += 1;
+                    lesserPoint.x = coordinateHelper.x; // now its shaped further
+
+                    // |x------|o-----o|-----x|
+                    if (greaterPoint.x > opposing.greaterPoint.x) {
+                        coordinateHelper = new Coordinate(lesserPoint);
+                        coordinateHelper.x = opposing.greaterPoint.x + 1;
+                        cubeHelper = new Cube(coordinateHelper, greaterPoint);
+                        newCubes.add(cubeHelper);
+                        coordinateHelper.x -= 1;
+                        greaterPoint.x = coordinateHelper.x; // shaped further
+                    }
+            } else {
+                // o-----x----o|----x|
+                if (greaterPoint.x > opposing.greaterPoint.x) {
+                    coordinateHelper = new Coordinate(lesserPoint);
+                    coordinateHelper.x = opposing.greaterPoint.x + 1;
+                    cubeHelper = new Cube(coordinateHelper, greaterPoint);
+                    newCubes.add(cubeHelper);
+                    coordinateHelper.x -= 1;
+                    greaterPoint.x = coordinateHelper.x; // shaped further
+                } else {
+                    // o----x-----x----o
+                }
+            }
+
+            if (lesserPoint.y < opposing.lesserPoint.y) {
+                // |y-----|o--y-----o
+                coordinateHelper = new Coordinate(greaterPoint);
+                coordinateHelper.y = opposing.lesserPoint.y - 1;
+                cubeHelper = new Cube(lesserPoint, coordinateHelper);
+                newCubes.add(cubeHelper);
+                coordinateHelper.y += 1;
+                lesserPoint.y = coordinateHelper.y; // now its shaped further
+
+                // |y------|o-----o|-----y|
+                if (greaterPoint.y > opposing.greaterPoint.y) {
+                    coordinateHelper = new Coordinate(lesserPoint);
+                    coordinateHelper.y = opposing.greaterPoint.y + 1;
+                    cubeHelper = new Cube(coordinateHelper, greaterPoint);
+                    newCubes.add(cubeHelper);
+                    coordinateHelper.y -= 1;
+                    greaterPoint.y = coordinateHelper.y; // shaped further
+                }
+            } else {
+                // o-----y----y|----o|
+                if (greaterPoint.y > opposing.greaterPoint.y) {
+                    coordinateHelper = new Coordinate(lesserPoint);
+                    coordinateHelper.y = opposing.greaterPoint.y + 1;
+                    cubeHelper = new Cube(coordinateHelper, greaterPoint);
+                    newCubes.add(cubeHelper);
+                    coordinateHelper.y -= 1;
+                    greaterPoint.y = coordinateHelper.y; // shaped further
+                } else {
+                    // o----y-----y----o
+                }
+            }
+
+            // check z
+            if (lesserPoint.z < opposing.lesserPoint.z) {
+                // |z-----|o--z-----o
+                coordinateHelper = new Coordinate(greaterPoint);
+                coordinateHelper.z = opposing.lesserPoint.z - 1;
+                cubeHelper = new Cube(lesserPoint, coordinateHelper);
+                newCubes.add(cubeHelper);
+                coordinateHelper.z += 1;
+                lesserPoint.z = coordinateHelper.z; // now its shaped further
+
+                // |z------|o-----o|-----z|
+                if (greaterPoint.z > opposing.greaterPoint.z) {
+                    coordinateHelper = new Coordinate(lesserPoint);
+                    coordinateHelper.z = opposing.greaterPoint.z + 1;
+                    cubeHelper = new Cube(coordinateHelper, greaterPoint);
+                    newCubes.add(cubeHelper);
+                    coordinateHelper.z -= 1;
+                    greaterPoint.z = coordinateHelper.z; // shaped further
+                }
+            } else {
+                // o-----z----z|----o|
+                if (greaterPoint.z > opposing.greaterPoint.z) {
+                    coordinateHelper = new Coordinate(lesserPoint);
+                    coordinateHelper.z = opposing.greaterPoint.z + 1;
+                    cubeHelper = new Cube(coordinateHelper, greaterPoint);
+                    newCubes.add(cubeHelper);
+                    coordinateHelper.z -= 1;
+                    greaterPoint.z = coordinateHelper.z; // shaped further
+                } else {
+                    // o----x-----x----o
+                }
+            }
+
+            return newCubes;
+        }
+
+        @Override
+        public String toString() {
+            return "Cube{" +
+                    "lesserPoint=" + lesserPoint +
+                    ", greaterPoint=" + greaterPoint +
+                    '}';
+        }
     }
 
     private class Coordinate {
@@ -60,6 +212,12 @@ public class App {
             this.x = x;
             this.y = y;
             this.z = z;
+        }
+
+        public Coordinate(Coordinate coordinate) {
+            x = coordinate.x;
+            y = coordinate.y;
+            z = coordinate.z;
         }
 
         @Override
@@ -80,6 +238,11 @@ public class App {
             result = 31 * result + y;
             result = 31 * result + z;
             return result;
+        }
+
+        @Override
+        public String toString() {
+            return x + "," + y + "," + z;
         }
     }
 }
